931fe2564504e25643bbf0d1c7033adfc4362529
ImJustAPassenger <ferrauto1020@gmail.com>
2024-10-22 17:41:12 +0200

    creation UI to fix

--- ChessApp/Pages/MainPage.xaml.cs
+++ ChessApp/Pages/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using ChessApp.Logic;
+﻿
+using ChessApp.Logic;
 using ChessApp.Models;
+using Microsoft.Maui.Controls.Shapes;
 
 namespace ChessApp.Pages;
 
@@ -7,12 +9,25 @@ public partial class MainPage : ContentPage
 {
 
 	private GameState gameState;
+	private Position selectedPos = null;
 	private readonly Image[,] pieceImages = new Image[8, 8];
+	private readonly Rectangle[,] highlights = new Rectangle[8, 8];
+	private readonly Dictionary<Position, Move> moveCache = new Dictionary<Position, Move>();
+
 	public MainPage()
 	{
 		InitializeComponent();
+		BoardGrid.SizeChanged += OnBoardGridSizeChanged;
 	}
+	private double squareWidth;
+	private double squareHeight;
 
+	private void OnBoardGridSizeChanged(object sender, EventArgs e)
+	{
+		// Calcola la dimensione di ciascun quadrato
+		squareWidth = BoardGrid.Width / 8;
+		squareHeight = BoardGrid.Height / 8;
+	}
 	protected override void OnAppearing()
 	{
 		InitializeBoard();
@@ -31,6 +46,10 @@ public partial class MainPage : ContentPage
 				Grid.SetRow(image, r);
 				Grid.SetColumn(image, c);
 				PieceGrid.Children.Add(image);
+
+				Rectangle highLight = new Rectangle();
+				highlights[r, c] = highLight;
+				HighLightGrid.Children.Add(highLight);
 			}
 		}
 	}
@@ -45,6 +64,87 @@ public partial class MainPage : ContentPage
 			}
 		}
 	}
+	private void CacheMoves(IEnumerable<Move> moves)
+	{
+		moveCache.Clear();
+		foreach (Move move in moves)
+		{
+			moveCache[move.ToPos] = move;
+		}
+	}
 
+	private void ShowHighLights()
+	{
+		Color color = Color.FromRgba(125, 125, 255, 150);
+		foreach (Position to in moveCache.Keys)
+		{
+			Console.WriteLine($"Highlighting position: Row={to.Row}, Column={to.Column}");
+			highlights[to.Row, to.Column].Fill = new SolidColorBrush(color);
+		}
+	}
+	private void HideHighLights()
+	{
+		foreach (Position to in moveCache.Keys)
+		{
+			highlights[to.Row, to.Column].Fill = Brush.Transparent;
+		}
+	}
+
+	private void BoardGrid_Tapped(object sender, TappedEventArgs e)
+	{
+		 var touchPoint = e.GetPosition(PieceGrid);
+    if (touchPoint.HasValue)
+    {
+		Console.WriteLine($"Touch Point: X={touchPoint.Value.X}, Y={touchPoint.Value.Y}");
+        
+        Position pos = ToSquarePosition(touchPoint.Value);
+        if (selectedPos == null)
+        {
+            OnFromPositionSelected(pos);
+        }
+        else
+        {
+            OnToPositionSelected(pos);
+        }
+    }
+	}
+
+	private void OnToPositionSelected(Position pos)
+	{
+		selectedPos = null;
+		HideHighLights();
+		if (moveCache.TryGetValue(pos, out Move move))
+		{
+			HandleMove(move);
+		}
+	}
+
+	private void HandleMove(Move move)
+	{
+		gameState.MakeMove(move);
+		DrawBoard(gameState.Board);
+	}
+
+	private void OnFromPositionSelected(Position pos)
+	{
+		IEnumerable<Move> moves = gameState.LegalMovesForPiece(pos);
+		if (moves.Any())
+		{
+			selectedPos = pos;
+			CacheMoves(moves);
+			ShowHighLights();
+		}
+	}
+
+	private Position ToSquarePosition(Point point)
+	{
+		int row = (int)(point.Y / squareHeight);
+		int col = (int)(point.X / squareWidth);
+
+		// Assicurati che le coordinate siano valide
+		row = Math.Clamp(row, 0, 7);
+		col = Math.Clamp(col, 0, 7);
+		return new Position(row, col);
+	}
 }
 
